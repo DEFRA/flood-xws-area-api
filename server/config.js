e2c0a89276f3c0b499b6e89bf6bddc18de5c3b47
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2021-08-24 10:13:16 +0100

    Revert changes for getting DB connection from JSON doc
    Worked out how to build a DB URI in CloudFormation so don't need to
    handle it in the application code.

--- server/config.js
+++ server/config.js
@@ -2,17 +2,6 @@ require('dotenv').config()
 const joi = require('joi')
 const envs = ['local', 'development', 'test', 'production']
 
-function getDatabaseUrl () {
-  // conditional so as not to break existing code but also handle copilot created connection credentials
-  // once all docker compose files are using process.env.DATABASE_CONNECTION then the conditional can be removed
-  if (process.env.DATABASE_CONNECTION) {
-    const { username, password, dbname, host, port } = JSON.parse(process.env.DATABASE_CONNECTION)
-    return `postgres://${username}:${password}@${host}:${port}/${dbname}`
-  } else {
-    return process.env.DATABASE_URL
-  }
-}
-
 // Define config schema
 const schema = joi.object().keys({
   env: joi.string().valid(...envs).default(envs[0]),
@@ -25,7 +14,7 @@ const config = {
   env: process.env.ENV,
   host: process.env.HOST,
   port: process.env.PORT,
-  databaseUrl: getDatabaseUrl()
+  databaseUrl: process.env.DATABASE_URL
 }
 
 // Validate config
